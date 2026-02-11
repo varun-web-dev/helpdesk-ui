60a6f6ef2fe0afe2cde772a5d3b4d1207c2c5929
Varun Pal <palvarun8368@gmail.com>
2026-02-11 10:56:49 +0530

    adding custom dropdown in ToDo buttton

--- src/components/RightPanel.jsx
+++ src/components/RightPanel.jsx
@@ -4,6 +4,7 @@ import { HiOutlineUserCircle } from "react-icons/hi";
 import FormDropdown from "./FormDropdown";
 import ExpandableRow from "./ExpandableRow";
 import { FiChevronDown } from "react-icons/fi";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const RightPanel = () => {
   const [priority, setPriority] = useState("Medium");
@@ -11,6 +12,9 @@ const RightPanel = () => {
   const [project, setProject] = useState("Administrative");
   const [type, setType] = useState("Task");
   const [reporter, setReporter] = useState("Allie Harmon");
+  const [status, setStatus] = useState("To Do");
+  const [openStatus, setopenStatus] = useState(false);
+
 
   return (
     <div className="w-[320px] overflow-y-auto flex flex-col gap-6 bg-white  border border-gray-300">
@@ -20,18 +24,95 @@ const RightPanel = () => {
 
         <div className="inline-flex">
           {/* ToDo Button */}
-          <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-1.5 rounded-l-md text-sm transition">
-            To Do
+          <button
+            onClick={() => setopenStatus(!openStatus)}
+            className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-1.5 rounded-l-md text-sm transition">
+            {status}
           </button>
 
-          <button className="bg-blue-600 hover:bg-blue-700 text-white px-2 rounded-r-md border-l border-blue-500 flex items-center transition">
+          <button
+            onClick={() => setopenStatus(!openStatus)}
+            className="bg-blue-600 hover:bg-blue-700 text-white px-2 rounded-r-md border-l border-blue-500 flex items-center transition">
             <FiChevronDown size={16} />
           </button>
         </div>
-
-        <RxCross2 className="text-gray-400 cursor-pointer hover:text-gray-600" size={18} />
-
+        <RxCross2 className="text-gray-500 cursor-pointer hover:text-gray-600" size={18} />
       </div>
+
+
+      {/* custom dropdown */}
+      {openStatus && (
+        <div className="absolute top-25 right-37 bg-white border border-gray-300 rounded-md shadow-lg z-20">
+
+          {/* Option 1 */}
+
+          <div onClick={() => {
+            setStatus("Work in Progress");
+            setopenStatus(false);
+          }}
+            className="flex items-start gap-2 px-3 py-2 cursor-pointer hover:bg-gray-50 group"
+          >
+
+            <span className="text-gray-500 group-hover:text-blue-600 transition">
+              <FaArrowRightLong />
+            </span>
+            <div>
+              <p className="text-sm font-medium ">
+                Work in Progress
+              </p>
+              <p className="text-sm text-gray-400">
+                in Progress
+              </p>
+            </div>
+          </div>
+
+          {/* Option 2 */}
+
+          <div onClick={() => {
+            setStatus("Work in Progress");
+            setopenStatus(false);
+          }}
+            className="flex items-start gap-2 px-3 py-2 cursor-pointer hover:bg-gray-50 group"
+          >
+
+            <span className="text-gray-500 group-hover:text-blue-600 transition">
+              <FaArrowRightLong />
+            </span>
+            <div>
+              <p className="text-sm font-medium ">
+                Nedeed Review
+              </p>
+              <p className="text-sm text-gray-400">
+                in Progress
+              </p>
+            </div>
+          </div>
+
+          {/* Option 3 */}
+
+          <div onClick={() => {
+            setStatus("Done");
+            setopenStatus(false);
+          }}
+            className="flex items-start gap-2 px-3 py-2 cursor-pointer hover:bg-gray-50 group"
+          >
+
+            <span className="text-gray-500 group-hover:text-blue-600 transition">
+              <FaArrowRightLong />
+            </span>
+            <div>
+              <p className="text-sm font-medium ">
+                Done
+              </p>
+              <p className="text-sm text-gray-400">
+                in Progress
+              </p>
+            </div>
+          </div>
+
+        </div>
+      )}
+
 
       {/* Dropdown Sections */}
       <div className="px-4 space-y-4  ">
